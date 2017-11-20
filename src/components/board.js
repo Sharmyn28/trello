8d4a6d06dba9a8bcfe938252efc6405293421e1d
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-11-20 07:01:43 -0500

    working add

--- src/components/board.js
+++ src/components/board.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../styles/style.css';
 import { NavLink } from 'react-router-dom';
-import { addComment } from '../actions/actions';
+import { readAllComments, addComment, nextTitle, handleLoginClick, handleLogoutClick } from '../actions/actions';
 import Head from './Head'
 import Footer from './footer'
 import { connect } from 'redux-zero/react';
 
-const User = ({ name }) => {
+const User = ({ name, ide, index }) => {
     return (
-        <div className="board">
+        <div className="board" id={ide}>
             <div className="inner">
-                <h4>{name}</h4>
+                <h4><NavLink onClick={()=> {nextTitle(index)}} to={'/details'}>{name}</NavLink></h4>
             </div>
         </div>
     );
@@ -20,14 +20,13 @@ const OtherBoard = ({ tboard }) => {
     return (
         <section>
             <header class="view-header">
-                <h3><i class="fa fa-users"></i><span> Other boards</span>
-                </h3>
+                <h3><i class="fa fa-users"></i><span> Other boards</span></h3>
             </header>
             <div className='boards-wrapper'>
                 {
                     tboard.map((list, index) => {
                         return (
-                            <div className='board'>
+                            <div className='board' key={index}>
                                 <div className='inner'>
                                     <h4>{list.name}</h4>
                                 </div>
@@ -40,7 +39,52 @@ const OtherBoard = ({ tboard }) => {
     )
 }
 
-const Boards = ({ tboard, board }) => {
+const LogoutButton = ({ showReply }) => {
+    return (
+        !showReply && <div className="board add-new">
+            <div className="inner">
+                <a id="add_new_board" onClick={() => handleLoginClick()}>Add new board...</a>
+            </div>
+        </div>
+    )
+}
+
+const LoginButton = ({ showReply }) => {
+    const onSubmit = e => {
+        e.preventDefault();
+        if (this.refInput.value) {
+            addComment(this.refInput.value);
+            this.refInput.value = '';
+        }
+
+    };
+    return (
+        !showReply && <div className='board form'>
+            <div className='inner'>
+                <h4>New board</h4>
+                <form onSubmit={onSubmit} id='new_board_form'>
+                    <div className="inner-wrap">
+                        <input
+                            type="text"
+                            id="board_name"
+                            name="name"
+                            placeholder="User"
+                            ref={e => (this.refInput = e)}
+                        />
+                        <button type="submit" name="submit">Create board</button>
+                        <span> or </span>
+                        <a onClick={() => handleLogoutClick()}>Cancel</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+readAllComments();
+
+const Boards = ({ tboard, board, showReply }) => {
     /*const onSubmit = e => {
         e.preventDefault();
         if (this.refInput.value) {
@@ -50,9 +94,12 @@ const Boards = ({ tboard, board }) => {
     }*/
 
     const BoardComponent = board.map((item, index) => {
+        
+
         return <User
             key={index}
             name={item.name}
+            index={index}
         />
     })
 
@@ -69,7 +116,9 @@ const Boards = ({ tboard, board }) => {
                                 </header>
                                 <div className="boards-wrapper">
                                     {BoardComponent}
-                                    <div className="board add-new">
+                                    <LogoutButton showReply={showReply} />
+                                    <LoginButton showReply={showReply} />
+                                    {/*<div className="board add-new">
                                         <div class="inner">
                                             <a id="add_new_board">Add new board...</a>
                                         </div>
@@ -91,8 +140,8 @@ const Boards = ({ tboard, board }) => {
                                                     <a >cancel</a>
                                                 </div>
     </form>*/}
-                                        </div>
-                                    </div>
+                                        {/*</div>
+                                    </div>*/}
                                 </div>
                             </section>
                             <OtherBoard tboard={tboard} />
